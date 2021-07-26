02712cd0c62159ba80327ee63cbe0ca4a55d2c8b
Rogério Soares <rogerio_si@yahoo.com.br>
2021-07-26 16:52:45 +0000

    details

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path("<int:pk>/", views.detail, name="detail"),
+
 ]
 
 
--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from loja.models import Project
 
-
 def home(request):
   project = Project.objects.all()
-  return render(request, 'index.html', {'projects':project})
+  context = {
+        'projects': project
+    }
+  return render(request, 'index.html', context)
+
+
+def detail(request,pk):
+  project = Project.objects.get(pk=pk)
+  context = {
+        'project': project
+    }
+  return render(request, 'detalhes.html', context)
+
+
